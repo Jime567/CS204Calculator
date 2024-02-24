eb3e2b04a2a97a19b4fb19c7a964aee247728687
James Phelps <james.elias.phelps@gmail.com>
2024-02-23 22:17:16 -0700

    Commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -38,7 +38,23 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            int prev1 = 1;
+            int prev2 = 1;
+            int fib = 0;
+            for (int i = 3; i <= n; i++) {
+                fib = prev1 + prev2;
+                prev1 = prev2;
+                prev2 = fib;
+            }
+            return fib;
+        }
+        
     }
 
 
@@ -50,7 +66,16 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int number){
-        return null;
+        if (number == 0) {
+            return "0";
+        }
+        StringBuilder binary = new StringBuilder();
+        while (number > 0) {
+            binary.insert(0, number % 2);
+            number = number / 2;
+        }
+        return binary.toString();
+
     }
 
     /*
@@ -62,7 +87,8 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-        return null;
+        return n + Math.random();
+        
     }
 
 
